14c90ddf8e5b58b1cb5750defa0101081044b4e4
Brad <brad@secretstudy.ca>
2023-04-21 15:20:43 -0700

    2m

--- server/monitor.js
+++ server/monitor.js
@@ -117,7 +117,7 @@ var jobMap = {
   },
   'xen': {
     enabled: true,
-    interval: '7 minutes',
+    interval: '2 minutes',
     fetch: () => {
       //      return xen.xenCheck([11], ["43114"]);
       return xen.xenCheck(_.shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]), _.shuffle(["43114", "137", "1284"]));
